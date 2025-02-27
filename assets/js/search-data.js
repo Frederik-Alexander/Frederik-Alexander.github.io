7f120d6be32ac47c11bca9258c09c972714baafc
Frederik-Alexander <34860099+Frederik-Alexander@users.noreply.github.com>
2025-02-27 14:07:46 +0000

    Deploying to gh-pages from @ Frederik-Alexander/Frederik-Alexander.github.io@6b5ae7960ea2abbe86bb83ea1067c097ac80aa79 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,6 +10,13 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-blog",
+          title: "Blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
